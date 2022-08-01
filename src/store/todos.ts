de696a8db0738c8848be428599452e744be51bbd
woong <inwoong100@gmail.com>
2022-08-01 23:34:14 +0900

    feature/tdooRecoil getTodo와 recoil 연동

--- src/store/todos.ts
+++ src/store/todos.ts
@@ -1,3 +1,6 @@
+import { atom, selector } from 'recoil';
+import { getTodos } from '../service/todos';
+import { dateState } from './date';
 export interface TodoLists {
   id: number;
   createdAt: string;
@@ -17,3 +20,17 @@ export interface Todo {
   imgURL: string;
   //taskId는 Todolists의 id와 동일
 }
+
+export const todoState = atom({
+  key: 'todoState',
+  default: [],
+});
+
+export const getTodoState = selector({
+  key: 'getTodoState',
+  get: async ({ get }) => {
+    const date = get(dateState);
+    const todos = await getTodos(date);
+    return todos;
+  },
+});
