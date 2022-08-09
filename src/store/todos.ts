5433ac62c37824cace9438c8ae1716c563f319d4
woong <inwoong100@gmail.com>
2022-08-09 23:58:34 +0900

    feature: pageNation 추가 (보이게만 구현)

--- src/store/todos.ts
+++ src/store/todos.ts
@@ -1,11 +1,4 @@
-import {
-  atom,
-  atomFamily,
-  selector,
-  selectorFamily,
-  useRecoilState,
-  useRecoilValueLoadable,
-} from 'recoil';
+import { atom, selector } from 'recoil';
 import { uid } from 'uid';
 import { getTodos } from '../service/todos';
 import { dateState } from './date';
@@ -34,14 +27,6 @@ export interface Todo {
   //taskId는 Todolists의 id와 동일
 }
 
-const replaceItemAtIndex = (
-  arr: Todo[],
-  index: number,
-  newValue: Todo
-): Todo[] => {
-  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
-};
-
 export const todoState = atom<Todo[]>({
   key: 'todoState',
   default: selector({
@@ -52,16 +37,6 @@ export const todoState = atom<Todo[]>({
       return todos as Todo[];
     },
   }),
-});
-
-export const todoTotal = selector<number[]>({
-  key: 'todosTotal',
-  get: ({ get }) => {
-    const todos = get(todoState);
-    const pageNum = Math.floor(Number(todos) / 7);
-    const pageArray = Array.from({ length: pageNum }, (_, idx) => idx + 1);
-    return pageArray;
-  },
 });
 
 export const addTodoStore = (todos: Todo[], date: string, todo: string) => {
@@ -80,29 +55,3 @@ export const addTodoStore = (todos: Todo[], date: string, todo: string) => {
 export const deleteTodoStore = (todos: Todo[], uuid: string) => {
   return todos.filter((todo) => todo.uuid !== uuid);
 };
-
-// export const getTodoState = selector<Todo[]>({
-//   key: 'todo/get',
-//   get: async ({ get }) => {
-//     const date = get(dateState);
-//     const todos = await getTodos(date);
-//     return todos as Todo[];
-//   },
-//   set: ({ set }, newValue) => {
-//     console.log('newValue', newValue);
-//     set(todoState, newValue);
-//   },
-// });
-// export const todoItemState = atomFamily<Todo[], string>({
-//   key: 'todoItemState',
-//   default: selectorFamily({
-//     key: 'todoItemState/default',
-//     get:
-//       () =>
-//       async ({ get }) => {
-//         const date = get(dateState);
-//         const data = await getTodos(date);
-//         return data as Todo[];
-//       },
-//   }),
-// });
